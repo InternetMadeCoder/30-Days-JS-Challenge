f56c75dac13e34a62b7485627ac8053b14b2e29c
Gayathri <anonymous210705@gmail.com>
2024-03-27 23:12:30 +0530

    Day 11

---
+++ Day 11/music-list.js
@@ -0,0 +1,81 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+/*
+teri tasveer
+nadaan parinde
+sapna
+saathi rey
+yaariyan
+choo lo
+raah de
+*/
+let allMusic = [
+    {
+      name: "Teri Tasveer",
+      artist: "Bayaan",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Nadaan Parinde",
+      artist: "Mohit Chauhan",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Sapna",
+      artist: "Bayaan",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Saathi Rey",
+      artist: "Arko",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Yaariyaan - Male Vocals",
+      artist: "Mohan Kannan, Pritam",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Choo Lo",
+      artist: "The Local Train",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+        name: "Raah De",
+        artist: "Rishi Kumar, Vaani Bhasin, Anshul Mathur",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Saath Chal - 1 Min Music",
+        artist: "Saahel",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Karoon Bhi Kya?",
+        artist: "Raghav Chaitanya",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Rut",
+        artist: "Navjot Ahuja",
+        img: "music-10",
+        src: "music-10"
+    },
+  ];
